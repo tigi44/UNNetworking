83eff74783e4c0d8c3312e05131e9bde49ae4848
tigi44 <onlytg@naver.com>
2020-06-02 13:56:22 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,11 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "UNNetworking",
-            dependencies: [],
-            path: "Sources"),
+            dependencies: []),
         .testTarget(
             name: "UNNetworkingTests",
-            dependencies: ["UNNetworking"],
-            path: "Tests"),
+            dependencies: ["UNNetworking"]),
     ]
 )
